87e0e5836d7c108de5936a1fb953f629d0bbee56
Yuyi Wan <ywan2@earth.crc.nd.edu>
2021-07-20 14:39:42 -0400

    add bash file

--- tests/run_over_testfiles.sh
+++ tests/run_over_testfiles.sh
@@ -2,3 +2,5 @@
 
 # Placeholder file for testing processor
 # Probably we can base this test on https://github.com/bu-cms/bucoffea/blob/master/test/run_over_testfiles.sh
+
+time python run.py ../../topcoffea/cfg/check_yields_sample.cfg -o ${OUT_FILE_NAME}
