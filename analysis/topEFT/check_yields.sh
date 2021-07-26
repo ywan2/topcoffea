2655337626e4f70f402a521abe11697081a7c4fa
Yuyi Wan <ywan2@earth.crc.nd.edu>
2021-07-26 15:09:06 -0400

    add conda

--- analysis/topEFT/check_yields.sh
+++ analysis/topEFT/check_yields.sh
@@ -25,19 +25,22 @@ conda activate coffea-env
 conda install -y python=3.8.3 six dill
 conda install -y -c conda-forge coffea ndcctools conda-pack xrootd uproot
 
+echo $?
 #conda run -n coffea-env python run.py ../../topcoffea/cfg/check_yields_sample.cfg -o ${OUT_FILE_NAME}
 
 # Run the processor
 printf "Running processor..."
 time python run.py ../../topcoffea/cfg/check_yields_sample.cfg -o ${OUT_FILE_NAME}
 
+echo $?
 # Make the jsons
 printf "Making yields json from pkl..."
 python get_yield_json.py -f histos/${OUT_FILE_NAME}.pkl.gz -n ${OUT_FILE_NAME} --quiet
 
+echo $?
 # If we want this to be the new ref json
 #cp ${OUT_FILE_NAME}.json tests/${REF_FILE_NAME}
-
+echo $?
 # Compare the yields to the ref json
 printf "Comparing yields agains reference..."
 python comp_yields.py ${REF_FILE_NAME} ${OUT_FILE_NAME}.json -t1 "Ref yields" -t2 "New yields" --quiet
