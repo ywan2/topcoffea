825e770f91472c52aebaf8d7e951c1eaddc5a2f3
Yuyi Wan <ywan2@earth.crc.nd.edu>
2021-07-27 10:09:51 -0400

    add conda

--- analysis/topEFT/check_yields.sh
+++ analysis/topEFT/check_yields.sh
@@ -33,13 +33,11 @@ printf "Running processor..."
 time python run.py ../../topcoffea/cfg/check_yields_sample.cfg -o ${OUT_FILE_NAME}
 
 echo $?
-printf "Check run.py"
 # Make the jsons
 printf "Making yields json from pkl..."
 python get_yield_json.py -f histos/${OUT_FILE_NAME}.pkl.gz -n ${OUT_FILE_NAME} --quiet
 
 echo $?
-printf "Check json.py"
 # If we want this to be the new ref json
 #cp ${OUT_FILE_NAME}.json tests/${REF_FILE_NAME}
 echo $?
@@ -48,6 +46,5 @@ printf "Comparing yields agains reference..."
 python comp_yields.py ${REF_FILE_NAME} ${OUT_FILE_NAME}.json -t1 "Ref yields" -t2 "New yields" --quiet
 
 # Do something with the exit code?
-printf "final check"
 echo $?
 
