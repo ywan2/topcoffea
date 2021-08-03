4ce3c3bd97e995b668090903fa8b5a5276c1e344
Yuyi Wan <ywan2@earth.crc.nd.edu>
2021-08-03 14:37:19 -0400

    test

--- analysis/topEFT/check_yields2.sh
+++ analysis/topEFT/check_yields2.sh
@@ -1,18 +1,1 @@
-# Activate uonda envirnoment
-printf "Activating conda environment..."
-
-export PYTHONPATH=
-
-# Activate the Conda shell hooks without starting a new shell.
-CONDA_BASE=$(conda info --base)
-. $CONDA_BASE/etc/profile.d/conda.sh
-
-echo "*** Install Conda and Pip packages"
-
-conda update -n base -c defaults conda
-conda create -y --name coffea-env
-conda activate coffea-env
-conda install -y python=3.8.3 six dill
-conda install -y -c conda-forge coffea ndcctools conda-pack xrootd uproot
-
-
+pip install -e .
