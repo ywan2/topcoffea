78c589181bc0dd1de47493dbe127dcf55599e55b
Yuyi Wan <ywan2@earth.crc.nd.edu>
2021-10-24 13:42:18 -0400

    test workqueue executor with bash file

--- analysis/topEFT/test.sh
+++ analysis/topEFT/test.sh
@@ -1,8 +1,3 @@
-echo hello world
-
-sleep 300 &
-
-echo hello world again
-
-#work_queue_worker -M $USER-workqueue-coffea --single-shot &
-#time python work_queue_run.py ../../topcoffea/json/test_samples/UL17_private_ttH_for_CI.json -o output_check_yields
+work_queue_worker -M $USER-workqueue-coffea --single-shot &
+time python work_queue_run.py ../../topcoffea/json/test_samples/UL17_private_ttH_for_CI.json
+# -o output_check_yields
