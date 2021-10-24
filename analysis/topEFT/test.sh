35649a095ff397ac510cbe33740bf0989826eaa6
Yuyi Wan <ywan2@earth.crc.nd.edu>
2021-10-24 14:17:35 -0400

    change command order on bash

--- analysis/topEFT/test.sh
+++ analysis/topEFT/test.sh
@@ -1,3 +1,3 @@
+time python work_queue_run.py ../../topcoffea/json/test_samples/UL17_private_ttH_for_CI.json &
 work_queue_worker -M $USER-workqueue-coffea --single-shot &
-time python work_queue_run.py ../../topcoffea/json/test_samples/UL17_private_ttH_for_CI.json
 # -o output_check_yields
