894a7e5190b91e46fc7b77e4faef0ba3ad1f29a7
Yuyi Wan <ywan2@earth.crc.nd.edu>
2021-10-25 13:16:59 -0400

    add bash file

--- analysis/topEFT/test.sh
+++ analysis/topEFT/test.sh
@@ -1,4 +1,6 @@
-work_queue_worker -d all -o worker.log localhost 9123 &
+work_queue_worker -M $USER-workqueue-coffea --single-shot &
 time python work_queue_run.py ../../topcoffea/json/test_samples/UL17_private_ttH_for_CI.json
+
+#work_queue_worker -d all -o worker.log localhost 9123 &
 #work_queue_worker -M $USER-workqueue-coffea --single-shot &
 # -o output_check_yields
