85a9565425d51d54711a90a7ac588b8b1b48b9c7
Yuyi Wan <ywan2@earth.crc.nd.edu>
2021-12-29 18:52:26 -0500

    test WQ

--- analysis/topEFT/ci.sh
+++ analysis/topEFT/ci.sh
@@ -1,2 +1,2 @@
-work_queue_worker -d all -o worker.log localhost 9125 &
+work_queue_worker -d all -o worker.log localhost 9123 &
 time python work_queue_run.py ../../topcoffea/json/test_samples/UL17_private_ttH_for_CI.json
